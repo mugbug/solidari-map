cf5edf4ba5f49f9cd47ef203a1542b0bf04966b9
Leonardo Saldanha <leoosaldanha@gmail.com>
2017-10-21 19:21:17 -0200

    Updated public folder and deployed version 0.2 to Firebase.

--- public/js/custom-map.js
+++ public/js/custom-map.js
@@ -16,7 +16,8 @@ function initMap() {
   geocoder = new google.maps.Geocoder();
   map = new google.maps.Map(document.getElementById('map'), {
     zoom: 14,
-    center: { lat: -22.2527245, lng: -45.7016189 }
+    center: { lat: -22.2527245, lng: -45.7016189 },
+    // styles: style.retro,
   });
 
   request.onreadystatechange = function () {
@@ -26,17 +27,22 @@ function initMap() {
 
       //Set markers
       for (var i in object) {
-        info = '<div id="content">' +
-          '<h5 id="firstHeading" class="firstHeading">' + object[i].name + '</h5>' +
-          '<div id="bodyContent">' +
-          '<p><b>Endereço:</b> ' + object[i].address + '</p>' +
-          '<p><b>Telefone:</b> ' + object[i].phone + '</p></div></div>';
-
+        info =
+        '<div class="panel panel-info">'+
+          '<div class="panel-heading">'+
+            '<h3 class="panel-title">'+object[i].name+'</h3>'+
+          '</div>'+
+          '<div class="panel-body">'+
+            '<p><b>Endereço:</b> ' + object[i].address + '</p>' +
+            '<p><b>Telefone:</b> ' + object[i].phone + '</p>' +
+          '</div>'+
+        '</div>';
         geocodeAddress(geocoder, map, object[i].address, object[i].name, infowindow, info);
       }
     }
   };
-  request.open("GET", "https://raw.githubusercontent.com/mugbug/fetin-2017/master/fetin2017/static/js/data.json", true);
+  // Change this URL when get final JSON data
+  request.open("GET", "https://raw.githubusercontent.com/mugbug/fetin-2017/master/utils/data.json", true);
   request.send();
 }
 
@@ -44,17 +50,26 @@ function initMap() {
 function geocodeAddress(geocoder, resultsMap, address, name, infowindow, info) {
   geocoder.geocode({ 'address': address }, function (results, status) {
     if (status === 'OK') {
-      // resultsMap.setCenter(results[0].geometry.location);
+      // demo icon
+        var icons = {
+          ong: {
+            icon: 'https://i.imgur.com/62hXFAC.png'
+          },
+        };
+
       var marker = new google.maps.Marker({
         map: resultsMap,
         position: results[0].geometry.location,
-        title: name
+        title: name,
+        icon: icons['ong'].icon
       });
       
       marker.addListener('click', function () {
         infowindow.setContent(info);
+        infowindow.setOptions({maxWidth:400});
         infowindow.open(map, this);
       });
+
     } else {
       alert('Geocode was not successful for the following reason: ' + status);
     }
@@ -91,4 +106,117 @@ function addressToCoordinates() {
       }
     });
   }, 1000);
+}
+
+var style = {
+  retro: [
+    {elementType: 'geometry', stylers: [{color: '#ebe3cd'}]},
+    {elementType: 'labels.text.fill', stylers: [{color: '#523735'}]},
+    {elementType: 'labels.text.stroke', stylers: [{color: '#f5f1e6'}]},
+    {
+      featureType: 'administrative',
+      elementType: 'geometry.stroke',
+      stylers: [{color: '#c9b2a6'}]
+    },
+    {
+      featureType: 'administrative.land_parcel',
+      elementType: 'geometry.stroke',
+      stylers: [{color: '#dcd2be'}]
+    },
+    {
+      featureType: 'administrative.land_parcel',
+      elementType: 'labels.text.fill',
+      stylers: [{color: '#ae9e90'}]
+    },
+    {
+      featureType: 'landscape.natural',
+      elementType: 'geometry',
+      stylers: [{color: '#dfd2ae'}]
+    },
+    {
+      featureType: 'poi',
+      elementType: 'geometry',
+      stylers: [{color: '#dfd2ae'}]
+    },
+    {
+      featureType: 'poi',
+      elementType: 'labels.text.fill',
+      stylers: [{color: '#93817c'}]
+    },
+    {
+      featureType: 'poi.park',
+      elementType: 'geometry.fill',
+      stylers: [{color: '#a5b076'}]
+    },
+    {
+      featureType: 'poi.park',
+      elementType: 'labels.text.fill',
+      stylers: [{color: '#447530'}]
+    },
+    {
+      featureType: 'road',
+      elementType: 'geometry',
+      stylers: [{color: '#f5f1e6'}]
+    },
+    {
+      featureType: 'road.arterial',
+      elementType: 'geometry',
+      stylers: [{color: '#fdfcf8'}]
+    },
+    {
+      featureType: 'road.highway',
+      elementType: 'geometry',
+      stylers: [{color: '#f8c967'}]
+    },
+    {
+      featureType: 'road.highway',
+      elementType: 'geometry.stroke',
+      stylers: [{color: '#e9bc62'}]
+    },
+    {
+      featureType: 'road.highway.controlled_access',
+      elementType: 'geometry',
+      stylers: [{color: '#e98d58'}]
+    },
+    {
+      featureType: 'road.highway.controlled_access',
+      elementType: 'geometry.stroke',
+      stylers: [{color: '#db8555'}]
+    },
+    {
+      featureType: 'road.local',
+      elementType: 'labels.text.fill',
+      stylers: [{color: '#806b63'}]
+    },
+    {
+      featureType: 'transit.line',
+      elementType: 'geometry',
+      stylers: [{color: '#dfd2ae'}]
+    },
+    {
+      featureType: 'transit.line',
+      elementType: 'labels.text.fill',
+      stylers: [{color: '#8f7d77'}]
+    },
+    {
+      featureType: 'transit.line',
+      elementType: 'labels.text.stroke',
+      stylers: [{color: '#ebe3cd'}]
+    },
+    {
+      featureType: 'transit.station',
+      elementType: 'geometry',
+      stylers: [{color: '#dfd2ae'}]
+    },
+    {
+      featureType: 'water',
+      elementType: 'geometry.fill',
+      stylers: [{color: '#b9d3c2'}]
+    },
+    {
+      featureType: 'water',
+      elementType: 'labels.text.fill',
+      stylers: [{color: '#92998d'}]
+    }
+  ],
 }
